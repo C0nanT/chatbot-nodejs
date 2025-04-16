7d81ede10cc939128aebdca4391bcad20f0bb76c
Conan Torres <conantorreswf@gmail.com>
2025-04-16 00:21:40 -0300

    style

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -5,16 +5,17 @@ class ApiService {
 	constructor() {
 		this.viaCepBaseUrl = "https://viacep.com.br/ws";
 		this.openMeteoBaseUrl = "https://api.open-meteo.com/v1/forecast";
-		this.geocodingBaseUrl = "https://geocoding-api.open-meteo.com/v1/search";
+		this.geocodingBaseUrl =
+			"https://geocoding-api.open-meteo.com/v1/search";
 
-        this.logManager = new LogManager();
+		this.logManager = new LogManager();
 	}
 
-    /**
-     * @description Faz uma consulta ao ViaCEP para obter informações de endereço a partir do CEP
-     * @param {string} cep - O CEP a ser consultado
-     * @return {Promise<Object>} - Os dados do endereço correspondente ao CEP
-     */
+	/**
+	 * @description Faz uma consulta ao ViaCEP para obter informações de endereço a partir do CEP
+	 * @param {string} cep - O CEP a ser consultado
+	 * @return {Promise<Object>} - Os dados do endereço correspondente ao CEP
+	 */
 	async consultCep(cep) {
 		try {
 			const formattedCep = cep.replace(/\D/g, "");
@@ -23,20 +24,20 @@ class ApiService {
 				`${this.viaCepBaseUrl}/${formattedCep}/json/`
 			);
 
-            if (response.data.erro) {
-                return {
-                    error: "CEP não encontrado. Verifique o número e tente novamente.",
-                };
-            }
+			if (response.data.erro) {
+				return {
+					error: "CEP não encontrado. Verifique o número e tente novamente.",
+				};
+			}
 
-            this.logManager.logAccess(
-                `Consulta de CEP realizada com sucesso: ${formattedCep}`
-            );
+			this.logManager.logAccess(
+				`Consulta de CEP realizada com sucesso: ${formattedCep}`
+			);
 
-            return response.data;
-        } catch (error) {
-            this.logManager.logError(error.message);
-            throw new Error("Failed to fetch CEP data");
+			return response.data;
+		} catch (error) {
+			this.logManager.logError(error.message);
+			throw new Error("Failed to fetch CEP data");
 		}
 	}
 
